fea30138cab757165d9703341f8a2c06759cff16
Hary Pham <harypham97@gmail.com>
2019-10-03 14:09:44 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+{
+
+/* ========================
+  Variables
+======================== */
+
+const FIREBASE_AUTH = firebase.auth();
+const FIREBASE_MESSAGING = firebase.messaging();
+const FIREBASE_DATABASE = firebase.database();
+
+const signInButton = document.getElementById('sign-in');
+const signOutButton = document.getElementById('sign-out');
+const subscribeButton = document.getElementById('subscribe');
+const unsubscribeButton = document.getElementById('unsubscribe');
+const sendNotificationForm = document.getElementById('send-notification-form');
+
+/* ========================
+  Event Listeners
+======================== */
+
+
+}
